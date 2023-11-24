fdae1e6c8a6c27f31114e0707d064201edab071b
mdcrystalll <dimaant20@gmail.com>
2023-11-24 10:05:48 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <h1>Прізвище: Антонюк</h1>
+          <h1>Ім'я: Дмитро</h1>
+          <h1>Група: 203</h1>
+      </div>
+  );
+}
+
+export default App;
